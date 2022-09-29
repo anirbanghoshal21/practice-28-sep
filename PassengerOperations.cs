fdc5fb1ba85f258e7bfb1180cdae1add83fa382d
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-29 16:23:57 +0530

    Add files via upload

---
+++ PassengerOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirwaysBusinessLibrary
+{
+    public class PassengerOperations
+    {
+        public List<Passenger> GetPassengers()
+
+            AirwaysDemoCnString context = new AirwaysDemoCnString();
+               
+    }
+}
